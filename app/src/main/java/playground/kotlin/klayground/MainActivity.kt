b6db142ac27c0152d3d46fb2797454746e517032
Vera Reynolds <vreynolds@pivotal.io>
2018-02-23 13:48:30 -0700

    first

---
+++ app/src/main/java/playground/kotlin/klayground/MainActivity.kt
@@ -0,0 +1,12 @@
+package playground.kotlin.klayground
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
